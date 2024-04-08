6f1f11374fb62408fe3443a83173779d7df6f73e
christian <kycdev@gmail.com>
2024-04-08 11:57:06 +0000

    refactoring

---
+++ src/main/java/org/cyk/system/poulsscolaire/client/fee/FeeCategoryCrudController.java
@@ -0,0 +1,57 @@
+package org.cyk.system.poulsscolaire.client.fee;
+
+import ci.gouv.dgbf.extension.primefaces.AbstractController;
+import ci.gouv.dgbf.extension.primefaces.crud.ListController;
+import ci.gouv.dgbf.extension.server.service.api.entity.AbstractIdentifiableCodableDto;
+import ci.gouv.dgbf.extension.server.service.api.entity.AbstractIdentifiableCodableNamableDto;
+import ci.gouv.dgbf.extension.server.service.api.entity.AbstractIdentifiableDto;
+import ci.gouv.dgbf.extension.server.service.api.request.ProjectionDto;
+import jakarta.enterprise.context.Dependent;
+import jakarta.inject.Inject;
+import lombok.Getter;
+import org.cyk.system.poulsscolaire.server.api.fee.FeeCategoryClient;
+import org.cyk.system.poulsscolaire.server.api.fee.FeeCategoryDto;
+import org.cyk.system.poulsscolaire.server.api.fee.FeeCategoryService;
+
+/**
+ * Cette classe représente le contrôleur de CRUD de {@link FeeCategoryDto}.
+ *
+ * @author Christian
+ *
+ */
+@Dependent
+public class FeeCategoryCrudController extends AbstractController {
+
+  @Inject
+  FeeCategoryClient client;
+
+  @Inject
+  @Getter
+  ListController listController;
+
+  @Override
+  protected void postConstruct() {
+    super.postConstruct();
+    name = "Rubriques";
+
+    listController.setEntityClass(FeeCategoryDto.class);
+    listController.setClient(client);
+    listController.setNotificationChannel(FeeCategoryService.PATH);
+
+    ProjectionDto projection = new ProjectionDto();
+    projection.addNames(AbstractIdentifiableDto.JSON_IDENTIFIER,
+        AbstractIdentifiableCodableDto.JSON_CODE, AbstractIdentifiableCodableNamableDto.JSON_NAME);
+    listController.getReadController().setProjection(projection);
+
+    listController.initialize();
+
+    listController.getCreateController()
+        .setFunction(entity -> client.create(((FeeCategoryDto) entity).getCode(),
+            ((FeeCategoryDto) entity).getName(), userIdentifier, null));
+
+    listController.getUpdateController()
+        .setFunction(entity -> client.update(((FeeCategoryDto) entity).getIdentifier(),
+            ((FeeCategoryDto) entity).getCode(), ((FeeCategoryDto) entity).getName(),
+            userIdentifier, null));
+  }
+}
---
+++ src/main/java/org/cyk/system/poulsscolaire/client/fee/FeeCategoryListPage.java
@@ -0,0 +1,30 @@
+package org.cyk.system.poulsscolaire.client.fee;
+
+import ci.gouv.dgbf.extension.primefaces.AbstractPage;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import lombok.Getter;
+
+/**
+ * Cette classe représente la page de liste de {@link FeeCategoryDto}.
+ *
+ * @author Christian
+ *
+ */
+@Named
+@ViewScoped
+public class FeeCategoryListPage extends AbstractPage {
+
+  @Inject
+  @Getter
+  FeeCategoryCrudController crudController;
+
+  @Override
+  protected void postConstruct() {
+    super.postConstruct();
+    contentTitle = "Liste des rubriques";
+  }
+  
+  public static final String OUTCOME = "feeCategoryListPage";
+}
