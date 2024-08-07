05f4c49ca6074b51be2f402aa7b62377d11d2e5a
Christian Yao Komenan <c.komenan@budget.gouv.ci>
2024-08-07 18:26:46 +0000

    refactoring

--- src/main/java/org/cyk/system/poulsscolaire/client/configuration/SchoolingController.java
+++ src/main/java/org/cyk/system/poulsscolaire/client/configuration/SchoolingController.java
@@ -1,6 +1,5 @@
 package org.cyk.system.poulsscolaire.client.configuration;
 
-import ci.gouv.dgbf.extension.core.Core;
 import ci.gouv.dgbf.extension.primefaces.AbstractController;
 import ci.gouv.dgbf.extension.primefaces.crud.ListController;
 import ci.gouv.dgbf.extension.server.service.api.entity.AbstractIdentifiableCodableDto;
@@ -9,8 +8,11 @@ import ci.gouv.dgbf.extension.server.service.api.request.ProjectionDto;
 import jakarta.enterprise.context.Dependent;
 import jakarta.inject.Inject;
 import lombok.Getter;
+import org.cyk.system.poulsscolaire.server.api.configuration.BranchFilter;
+import org.cyk.system.poulsscolaire.server.api.configuration.PeriodFilter;
 import org.cyk.system.poulsscolaire.server.api.configuration.SchoolingClient;
 import org.cyk.system.poulsscolaire.server.api.configuration.SchoolingDto;
+import org.cyk.system.poulsscolaire.server.api.configuration.SchoolingRequestMapper;
 import org.cyk.system.poulsscolaire.server.api.configuration.SchoolingService;
 import org.cyk.system.poulsscolaire.server.api.configuration.SchoolingService.SchoolingCreateRequestDto;
 import org.cyk.system.poulsscolaire.server.api.configuration.SchoolingService.SchoolingUpdateRequestDto;
@@ -41,13 +43,19 @@ public class SchoolingController extends AbstractController {
 
   @Inject
   @Getter
+  BranchSelectOne branchSelectOne;
+
+  @Inject
+  SchoolingRequestMapper requestMapper;
+
+  @Inject
+  @Getter
   SchoolingFilterController filterController;
 
   @Override
   protected void postConstruct() {
     super.postConstruct();
     name = "Scolarités";
-    initialize();
   }
 
   /**
@@ -60,12 +68,10 @@ public class SchoolingController extends AbstractController {
     listController.setFilterController(filterController);
 
     ProjectionDto projection = new ProjectionDto();
-    Core.runIfNull(filterController.getFilter().getSchoolIdentifier(), () -> {
-      projection.addNames(SchoolingDto.JSON_SCHOOL_AS_STRING);
-    });
-    Core.runIfNull(filterController.getFilter().getPeriodIdentifier(), () -> {
-      projection.addNames(SchoolingDto.JSON_PERIOD_AS_STRING);
-    });
+    projection.addNamesIfStringBlank(filterController.getFilter().getSchoolIdentifier(),
+        SchoolingDto.JSON_SCHOOL_AS_STRING);
+    projection.addNamesIfStringBlank(filterController.getFilter().getPeriodIdentifier(),
+        SchoolingDto.JSON_PERIOD_AS_STRING);
     projection.addNames(AbstractIdentifiableDto.JSON_IDENTIFIER,
         AbstractIdentifiableCodableDto.JSON_CODE, SchoolingDto.JSON_BRANCH_AS_STRING,
         SchoolingDto.JSON_PRE_REGISTRATION_AMOUNT_AS_STRING,
@@ -81,26 +87,52 @@ public class SchoolingController extends AbstractController {
     listController.getGotoReadPageButton().setOutcome(SchoolingReadPage.OUTCOME);
 
     listController.getCreateController().setFunction(entity -> {
-      SchoolingCreateRequestDto request = new SchoolingCreateRequestDto();
-      request.setSchoolIdentifier(((SchoolingDto) entity).getSchoolIdentifier());
-      request.setPeriodIdentifier(((SchoolingDto) entity).getPeriodIdentifier());
-      request.setBranchIdentifier(((SchoolingDto) entity).getBranchIdentifier());
-      request.setPreRegistrationAmount(((SchoolingDto) entity).getPreRegistrationAmount());
+      SchoolingCreateRequestDto request = requestMapper.mapCreate((SchoolingDto) entity);
       request.setAuditWho(userIdentifier);
       return client.create(request);
     });
 
-    listController.getUpdateController().setProjection(new ProjectionDto().addNames(
-        AbstractIdentifiableDto.JSON_IDENTIFIER, SchoolingDto.JSON_PRE_REGISTRATION_AMOUNT));
+    listController.getUpdateController()
+        .setProjection(new ProjectionDto().addNames(AbstractIdentifiableDto.JSON_IDENTIFIER,
+            SchoolingDto.JSON_SCHOOL_IDENTIFIER, SchoolingDto.JSON_PERIOD_IDENTIFIER,
+            SchoolingDto.JSON_BRANCH_IDENTIFIER, SchoolingDto.JSON_PRE_REGISTRATION_AMOUNT));
+
     listController.getUpdateController().setFunction(entity -> {
-      SchoolingUpdateRequestDto request = new SchoolingUpdateRequestDto();
-      request.setIdentifier(((SchoolingDto) entity).getIdentifier());
-      request.setSchoolIdentifier(((SchoolingDto) entity).getSchoolIdentifier());
-      request.setPeriodIdentifier(((SchoolingDto) entity).getPeriodIdentifier());
-      request.setBranchIdentifier(((SchoolingDto) entity).getBranchIdentifier());
-      request.setPreRegistrationAmount(((SchoolingDto) entity).getPreRegistrationAmount());
+      SchoolingUpdateRequestDto request = requestMapper.mapUpdate((SchoolingDto) entity);
       request.setAuditWho(userIdentifier);
       return client.update(request);
     });
+
+    schoolSelectOne.getSelectOneMenu()
+        .addValueConsumer(identifier -> listController
+            .getCreateControllerOrUpdateControllerEntityAs(SchoolingDto.class)
+            .setSchoolIdentifier(identifier));
+
+    schoolSelectOne.getSelectOneMenu().valueChangeAjax().setRunnable(() -> {
+      PeriodFilter periodFilter = new PeriodFilter();
+      periodFilter.setSchoolIdentifier(schoolSelectOne.getSelectOneMenu().getValue());
+      periodFilter.setOpened(true);
+      periodSelectOne.setFilter(periodFilter.toDto());
+      periodSelectOne.computeSelectOneMenuChoices();
+
+      BranchFilter branchFilter = new BranchFilter();
+      branchFilter.setSchoolIdentifier(schoolSelectOne.getSelectOneMenu().getValue());
+      branchSelectOne.setFilter(branchFilter.toDto());
+      branchSelectOne.computeSelectOneMenuChoices();
+    });
+    schoolSelectOne.getSelectOneMenu().valueChangeAjax().setDisabled(false);
+    schoolSelectOne.getSelectOneMenu().valueChangeAjax()
+        .setUpdate(periodSelectOne.getSelectOneMenu().getIdentifier() + ","
+            + branchSelectOne.getSelectOneMenu().getIdentifier());
+
+    periodSelectOne.getSelectOneMenu()
+        .addValueConsumer(identifier -> listController
+            .getCreateControllerOrUpdateControllerEntityAs(SchoolingDto.class)
+            .setPeriodIdentifier(identifier));
+
+    branchSelectOne.getSelectOneMenu()
+        .addValueConsumer(identifier -> listController
+            .getCreateControllerOrUpdateControllerEntityAs(SchoolingDto.class)
+            .setBranchIdentifier(identifier));
   }
 }
