5dd2c842bb45003ef607c28468dbb5d724ae9800
Christian Yao Komenan <c.komenan@budget.gouv.ci>
2024-08-09 17:58:01 +0000

    Publication du client du module de pouls-scolaire bas├® sur Quarkus et Primefaces v0.0.0-202408091758

--- script/git/commit_push_to_dev_deploy.bat
+++ script/git/commit_push_to_dev_deploy.bat
@@ -1,2 +1,2 @@
 call commit_push_to_dev.bat
-call sigctl pipeline -u http://git.dgbf.ci/christian/mic-pouls-scolaire -b develop -j 17 -e dev -c k1 -m srvce-cption-dev-sol-lgciel@gouvci.onmicrosoft.com
+call sigctl pipeline -u http://git.dgbf.ci/christian/mic-pouls-paid -b develop -j 17 -e dev -c k1 -m srvce-cption-dev-sol-lgciel@gouvci.onmicrosoft.com
