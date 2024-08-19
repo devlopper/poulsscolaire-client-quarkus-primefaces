52cc3a69ea0b164c33275f0625a8d130e68f6af8
Christian Yao Komenan <c.komenan@budget.gouv.ci>
2024-08-19 09:28:25 +0000

    refactoring

--- src/main/java/org/cyk/system/poulsscolaire/client/AuthorizationFilter.java
+++ src/main/java/org/cyk/system/poulsscolaire/client/AuthorizationFilter.java
@@ -51,12 +51,18 @@ public class AuthorizationFilter implements Filter {
     cashierRoleCode = "14";
     debtCollectorRoleCode = "15";
     map = new HashMap<>();
+    
     allowSession(founderRoleCode, managerRoleCode, accountantRoleCode);
+    
     allowFeeCategory(founderRoleCode, managerRoleCode, accountantRoleCode);
+    allowFee(founderRoleCode, managerRoleCode, accountantRoleCode);
+    allowAmountDeadline(founderRoleCode, managerRoleCode, accountantRoleCode);
     allowSchooling(founderRoleCode, managerRoleCode, accountantRoleCode);
+    
     allowStudent(founderRoleCode, managerRoleCode, accountantRoleCode);
     allowRegistration(founderRoleCode, managerRoleCode, accountantRoleCode);
     allowAdjustedFee(founderRoleCode, managerRoleCode, accountantRoleCode);
+    
     allowPayment(founderRoleCode, managerRoleCode, accountantRoleCode);
   }
 
@@ -81,6 +87,14 @@ public class AuthorizationFilter implements Filter {
     allow("/private/feecategory/administrator/list.xhtml", rolesCodes);
   }
 
+  void allowFee(String... rolesCodes) {
+    allow("/private/fee/administrator/list/index.xhtml", rolesCodes);
+  }
+  
+  void allowAmountDeadline(String... rolesCodes) {
+    allow("/private/amountdeadline/administrator/list/index.xhtml", rolesCodes);
+  }
+  
   void allowSchooling(String... rolesCodes) {
     allow("/private/schooling/accountant/list.xhtml", rolesCodes);
   }
