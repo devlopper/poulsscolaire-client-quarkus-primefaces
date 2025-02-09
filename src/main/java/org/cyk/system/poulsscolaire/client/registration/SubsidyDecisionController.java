18ac14b851ff1aa59646ea60281028453ff84775
Christian Yao Komenan <c.komenan@budget.gouv.ci>
2025-02-09 16:41:35 +0000

    refactoring

--- src/main/java/org/cyk/system/poulsscolaire/client/registration/SubsidyDecisionController.java
+++ src/main/java/org/cyk/system/poulsscolaire/client/registration/SubsidyDecisionController.java
@@ -1,14 +1,16 @@
 package org.cyk.system.poulsscolaire.client.registration;
 
 import ci.gouv.dgbf.extension.primefaces.AbstractController;
+import ci.gouv.dgbf.extension.primefaces.component.input.InputNumberController;
+import ci.gouv.dgbf.extension.primefaces.component.input.InputTextController;
 import ci.gouv.dgbf.extension.primefaces.crud.ListController;
 import ci.gouv.dgbf.extension.server.service.api.entity.AbstractIdentifiableCodableDto;
-import ci.gouv.dgbf.extension.server.service.api.entity.AbstractIdentifiableCodableNamableDto;
 import ci.gouv.dgbf.extension.server.service.api.entity.AbstractIdentifiableDto;
 import ci.gouv.dgbf.extension.server.service.api.request.ProjectionDto;
 import jakarta.enterprise.context.Dependent;
 import jakarta.inject.Inject;
 import lombok.Getter;
+import org.cyk.system.poulsscolaire.client.configuration.SchoolingSelectOneController;
 import org.cyk.system.poulsscolaire.server.api.registration.SubsidyDecisionClient;
 import org.cyk.system.poulsscolaire.server.api.registration.SubsidyDecisionDto;
 import org.cyk.system.poulsscolaire.server.api.registration.SubsidyDecisionRequestMapper;
@@ -35,12 +37,24 @@ public class SubsidyDecisionController extends AbstractController {
   @Getter
   ListController listController;
 
+  @Inject
+  @Getter
+  InputTextController codeInputTextController;
+
+  @Inject
+  @Getter
+  SchoolingSelectOneController schoolingSelectOneController;
+
+  @Inject
+  @Getter
+  InputNumberController amountInputNumberController;
+
   @Override
   protected void postConstruct() {
     super.postConstruct();
     name = SubsidyDecisionDto.NAME;
   }
-  
+
   /**
    * Initialiser.
    */
@@ -49,9 +63,9 @@ public class SubsidyDecisionController extends AbstractController {
     listController.setClient(client);
     listController.setNotificationChannel(SubsidyDecisionService.PATH);
 
-    ProjectionDto projection = new ProjectionDto();
-    projection.addNames(AbstractIdentifiableDto.JSON_IDENTIFIER,
-        AbstractIdentifiableCodableDto.JSON_CODE, AbstractIdentifiableCodableNamableDto.JSON_NAME);
+    ProjectionDto projection = new ProjectionDto().addNames(AbstractIdentifiableDto.JSON_IDENTIFIER,
+        AbstractIdentifiableCodableDto.JSON_CODE, SubsidyDecisionDto.JSON_SCHOOLING_AS_STRING,
+        SubsidyDecisionDto.JSON_AMOUNT_AS_STRING);
     listController.getReadController().setProjection(projection);
 
     listController.initialize();
@@ -63,11 +77,38 @@ public class SubsidyDecisionController extends AbstractController {
       return client.create(request);
     });
 
+    listController.getUpdateController()
+        .setProjection(new ProjectionDto().addNames(AbstractIdentifiableDto.JSON_IDENTIFIER,
+            AbstractIdentifiableCodableDto.JSON_CODE, SubsidyDecisionDto.JSON_SCHOOLING_IDENTIFIER,
+            SubsidyDecisionDto.JSON_AMOUNT));
+
+    listController.getUpdateController().addEntityConsumer(entity -> {
+      codeInputTextController.getInputText().writeValue(((SubsidyDecisionDto) entity).getCode());
+      schoolingSelectOneController.getSelectOneMenu()
+          .writeValue(((SubsidyDecisionDto) entity).getSchoolingIdentifier());
+      amountInputNumberController.getInputInteger()
+          .writeValue(((SubsidyDecisionDto) entity).getAmount());
+    });
+
     listController.getUpdateController().setFunction(entity -> {
       SubsidyDecisionUpdateRequestDto request =
           requestMapper.mapUpdate((SubsidyDecisionDto) entity);
       request.setAuditWho(userIdentifier);
       return client.update(request);
     });
+
+    codeInputTextController.setOutputLableValue("Numéro");
+    codeInputTextController.getInputText().addValueConsumer(code -> listController
+        .getCreateControllerOrUpdateControllerEntityAs(SubsidyDecisionDto.class).setCode(code));
+
+    schoolingSelectOneController.getSelectOneMenu()
+        .addValueConsumer(identifier -> listController
+            .getCreateControllerOrUpdateControllerEntityAs(SubsidyDecisionDto.class)
+            .setSchoolingIdentifier(identifier));
+    schoolingSelectOneController.getSelectOneMenu().getOutputLabel().setValue("Branche");
+    
+    amountInputNumberController.setOutputLableValue("Montant");
+    amountInputNumberController.getInputInteger().addValueConsumer(amount -> listController
+        .getCreateControllerOrUpdateControllerEntityAs(SubsidyDecisionDto.class).setAmount(amount));
   }
 }
